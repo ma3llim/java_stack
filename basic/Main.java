0d8400c889648a5303610034ceaac46d961b0d1c
ma3llim007 <mohdsameer68257@gmail.com>
2026-01-11 19:27:58 +0530

    chore: learn print-out and comments

--- basic/Main.java
+++ basic/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Test");
-        System.out.println("Test 2");
+        // Todo: Remove the Dymmanic Output
+        System.out.print("Test ");
+        System.out.print("Test 2");
     }
 }
