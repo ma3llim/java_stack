cfbb5f92371187e063cca93e1d1d21ea16eba8f8
ma3llim007 <mohdsameer68257@gmail.com>
2026-02-03 23:34:00 +0530

    chore: learn the operators

---
+++ basic/operators.java
@@ -0,0 +1,38 @@
+import java.text.ListFormat.Style;
+
+public class operators {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        // Arithmetic operators
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println((double) a / b);
+        System.out.println(a % b);
+
+        // Relational operators
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+        // Assignment operators
+        int x = 10;
+        x %= 5;
+        System.out.println(x);
+
+        // Logical operators
+        System.out.println(2 > 3 && 2 > 5);
+
+        // Ternary operators
+        int number = 10;
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println(result);
+
+        // Setting final keyword so user can't update update or change the things
+        final float PI = 3.15f;
+        System.out.print(PI);
+    }
+}
