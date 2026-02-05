294dabb38a04d85d86e1fbf70d9b6625a017e8aa
ma3llim <mohdsameer68257@gmail.com>
2026-02-05 23:50:12 +0530

    chore: learn the how to get user input

---
+++ basic/UserInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Good Morning " + name);
+
+        System.out.println("Enter your age: ");
+        int age = input.nextInt();
+        System.out.println(age);
+    }
+}
