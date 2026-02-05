25fea362cc78a20f1b506f2992f34672a501c30b
ma3llim <mohdsameer68257@gmail.com>
2026-02-05 23:40:11 +0530

    chore: learn the escape

---
+++ basic/Escape.java
@@ -0,0 +1,9 @@
+public class Escape {
+    public static  void main (String[] args){
+        System.out.println("Hello \"World\"...");
+        System.out.println("One \n Two");
+        System.out.println("Three \t Four");
+        System.out.println("Five \'Six\'");
+        System.out.println("Seven \\Eight\\");
+    }
+}
