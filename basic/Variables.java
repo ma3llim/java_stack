be4b363f62101be9a9c66fc9b960fd9a2fbc87fc
ma3llim007 <mohdsameer68257@gmail.com>
2026-02-05 23:18:10 +0530

    chore: learn the java

--- basic/Variables.java
+++ basic/Variables.java
@@ -1,6 +1,17 @@
 public class Variables {
     public static void main(String[] args){
-        int a =2;
-        System.out.println(a);
+        int myNumber = 865;
+        int yourNumber;
+        yourNumber = 45;
+        boolean isVegetarian = true;
+        float myFloat = 5.0f;
+        double myDouble = 6.87789789;
+        String str = "Hello World";
+
+        System.out.println("Int " + yourNumber);
+        System.out.println("Boolean " + isVegetarian);
+        System.out.println("Float " + myFloat);
+        System.out.println("Double " + myDouble);
+        System.out.println("String " + str);
     }
 }
