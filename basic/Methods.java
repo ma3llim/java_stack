61e401ceeb72fc6fca7d1dc7746f3072e4adca14
ma3llim <mohdsameer68257@gmail.com>
2026-02-08 15:09:45 +0530

    chore: learn the methods with parameter and arguments

---
+++ basic/Methods.java
@@ -0,0 +1,9 @@
+public class Methods {
+    public static void main(String[] args){
+        greetWelcome();
+    }
+
+    public static void greetWelcome(){
+        System.out.println("Welcome User");
+    }
+}
