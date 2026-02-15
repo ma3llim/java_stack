d7d27f8fef5fb12cb7b4e446afb1fa6e65d875fe
ma3llim <mohdsameer68257@gmail.com>
2026-02-15 21:09:27 +0530

    chore: practice and learn switch and ternary operators

---
+++ basic/ControlSatements/TernaryOperation.java
@@ -0,0 +1,10 @@
+package ControlSatements;
+
+public class TernaryOperation {
+    public static void main(String[] args) {
+        int number = 40;
+        // TernaryOperation
+        String result = number > 30 ? "40 Greater than 30" : "30 Less than 40";
+        System.out.println(result);
+    }
+}
