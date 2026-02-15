21a48d780079cff40a800c7a283b5ecc83af8f72
ma3llim <mohdsameer68257@gmail.com>
2026-02-15 21:58:29 +0530

    chore: learn the maths class

---
+++ basic/MathClass.java
@@ -0,0 +1,14 @@
+public class MathClass {
+    public static void main(String[] args) {
+        System.out.println("Absolute Value: " + Math.abs(-4));
+        System.out.println("Rounds Up: " + Math.ceil(4.4));
+        System.out.println("Rounds Down: " + Math.floor(4.9));
+        System.out.println("Rounds to nearest integer: " + Math.round(10.9));
+
+        // System.out.println(": " + Math.());
+        // System.out.println(": " + Math.());
+        // System.out.println(": " + Math.());
+        // System.out.println(": " + Math.());
+        // System.out.println(": " + Math.());
+    }
+}
