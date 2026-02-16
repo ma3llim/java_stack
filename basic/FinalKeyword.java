f22046ae62a3a42eea09d3ed948e78e0556f3d73
ma3llim <mohdsameer68257@gmail.com>
2026-02-16 23:05:46 +0530

    chore: learn the final keyword concept

---
+++ basic/FinalKeyword.java
@@ -0,0 +1,11 @@
+public class FinalKeyword {
+    public final String NAME = "Mohd Sameer";
+
+    void updateName() {
+        name = "MD Sameer";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ basic/StringBuilder.java
@@ -0,0 +1,11 @@
+public class StringBuilder {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("First");
+        sb.append(" Second");
+        sb.append(" Third");
+        sb.append(" Four");
+        sb.append(" Five");
+
+        System.out.println(sb);
+    }
+}
