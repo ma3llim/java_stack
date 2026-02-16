94f9a2c3a43d4a14bda79d35757edfa82be40a77
ma3llim <mohdsameer68257@gmail.com>
2026-02-16 22:57:30 +0530

    chore: learn the toString methods

---
+++ basic/toString.java
@@ -0,0 +1,33 @@
+// toString Methods
+class Car {
+    int noOfWheels, noOfDoors;
+    float maxSpeed;
+    String name, modelNumber, company;
+
+    public Car(int noOfWheels, int noOfDoors, float maxSpeed, String name, String modelNumber, String company) {
+        this.noOfWheels = noOfWheels;
+        this.noOfDoors = noOfDoors;
+        this.maxSpeed = maxSpeed;
+        this.name = name;
+        this.modelNumber = modelNumber;
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return "Car Details:\n" +
+                "Company: " + company + "\n" +
+                "Name: " + name + "\n" +
+                "Model Number: " + modelNumber + "\n" +
+                "Wheels: " + noOfWheels + "\n" +
+                "Doors: " + noOfDoors + "\n" +
+                "Max Speed: " + maxSpeed + " km/h";
+    }
+}
+
+public class toString {
+    public static void main(String[] args) {
+        Car swift = new Car(4, 4, 120, "Swift", "sw879", "Maruti");
+        System.out.println(swift.toString());
+    }
+}
