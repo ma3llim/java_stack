1c81193db59c08ba7f4457d232ce09fc116e9bb0
ma3llim <mohdsameer68257@gmail.com>
2026-02-16 22:08:20 +0530

    chore: learn the maths few more methods

--- basic/MathClass.java
+++ basic/MathClass.java
@@ -4,11 +4,12 @@ public class MathClass {
         System.out.println("Rounds Up: " + Math.ceil(4.4));
         System.out.println("Rounds Down: " + Math.floor(4.9));
         System.out.println("Rounds to nearest integer: " + Math.round(10.9));
-
-        // System.out.println(": " + Math.());
-        // System.out.println(": " + Math.());
-        // System.out.println(": " + Math.());
-        // System.out.println(": " + Math.());
-        // System.out.println(": " + Math.());
+        System.out.println("Maximun: " + Math.max(10, 64));
+        System.out.println("Minimum: " + Math.min(2, 9));
+        System.out.println("Power Calcluation: " + Math.pow(5, 2));
+        System.out.println("Square Root: " + Math.sqrt(4));
+        System.out.println("Random Number Generation: " + Math.random());
+        System.out.println("Exponential function: " + Math.exp(4));
+        System.out.println("Logarithmic function: " + Math.log(4));
     }
 }
