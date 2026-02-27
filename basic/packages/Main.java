8588959604c855ba4a10007b45b588354e2adcae
ma3llim <mohdsameer68257@gmail.com>
2026-02-27 20:35:14 +0530

    chore: learn the packages

---
+++ basic/packages/Main.java
@@ -0,0 +1,10 @@
+package packages;
+
+import packages.exampele.connection;
+
+public class Main {
+    public static void main(String[] args) {
+        connection test = new connection();
+        System.out.println(test);
+    }
+}
---
+++ basic/packages/exampele/connection.java
@@ -0,0 +1,7 @@
+package packages.exampele;
+
+public class connection {
+    public static void main(String[] args) {
+        System.out.println("Database is successfully connected");
+    }
+}
