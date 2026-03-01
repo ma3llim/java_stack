2bcde157405558354bbb0b3f6ae844f9df934241
ma3llim <mohdsameer68257@gmail.com>
2026-03-01 11:31:20 +0530

    chore: learn the enum class

---
+++ advJava/enumClass.java
@@ -0,0 +1,25 @@
+enum Laptop {
+    Macbook(2000), XPS(2200), Surface(1500), ThinkPad(1800);
+
+    private int price;
+
+    private Laptop(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
+
+public class enumClass {
+    public static void main(String[] args) {
+        Laptop lap = Laptop.Macbook;
+        System.out.println(lap.getPrice());
+    }
+}
