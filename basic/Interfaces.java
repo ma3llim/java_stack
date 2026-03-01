e61249d3cd87728aef09c381042f9d95d441603b
ma3llim <mohdsameer68257@gmail.com>
2026-03-01 11:17:50 +0530

    chore: learn the interface concepts

---
+++ basic/Interfaces.java
@@ -0,0 +1,39 @@
+interface A {
+    String name = "Mohd Sameer"; // final and static
+
+    void show();
+
+    void config();
+}
+
+interface X {
+    void run();
+}
+
+interface Y extends A, X {
+
+}
+
+class B implements Y {
+    public void show() {
+        System.out.println("In Show Method");
+    }
+
+    public void config() {
+        System.out.println("In config Method");
+    }
+
+    public void run() {
+        System.out.println("In run Method");
+    }
+}
+
+public class Interfaces {
+    public static void main(String[] args) {
+        A obj;
+        obj = new B();
+        obj.show();
+        obj.config();
+        System.out.println(A.name);
+    }
+}
---
+++ basic/NeedofInterface.java
@@ -0,0 +1,33 @@
+interface Computer {
+    void code();
+}
+
+class Desktop implements Computer {
+    public void code() {
+        System.out.println("Code, Complie, Run: Faster");
+    }
+}
+
+class Laptop implements Computer {
+    public void code() {
+        System.out.println("Code, Complie, Run");
+    }
+
+}
+
+class Developer {
+    public void devApp(Computer device) {
+        device.code();
+    }
+}
+
+public class NeedofInterface {
+    public static void main(String[] args) {
+        Computer desktop = new Desktop();
+        Computer laptop = new Laptop();
+        Developer newDev = new Developer();
+
+        newDev.devApp(desktop);
+        newDev.devApp(laptop);
+    }
+}
