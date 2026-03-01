5eabe18660a3744be1ac482cb2c8f21fc91cb6c4
ma3llim <mohdsameer68257@gmail.com>
2026-03-01 12:46:31 +0530

    chore: learn lambda function returns

---
+++ advJava/LambdaReturn.java
@@ -0,0 +1,12 @@
+@FunctionalInterface
+interface utils {
+    int add(int num1, int num2);
+}
+
+public class LambdaReturn {
+    public static void main(String[] args) {
+        utils obj = (int num1, int num2) -> num1 + num2;
+        int result = obj.add(10, 20);
+        System.out.println("Sum of both sum: " + result);
+    }
+}
