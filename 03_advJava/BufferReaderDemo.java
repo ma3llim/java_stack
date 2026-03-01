9e5b5a7e10996f058272e4f711c30a1fc2e20574
ma3llim <mohdsameer68257@gmail.com>
2026-03-01 21:29:42 +0530

    chore: learn how developer takes input from user with BufferedReader

---
+++ 03_advJava/BufferReaderDemo.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferReaderDemo {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter a number: ");
+
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bufferR = new BufferedReader(in);
+
+        String num = bufferR.readLine();
+        System.out.println(num);
+
+        bufferR.close();
+    }
+}
