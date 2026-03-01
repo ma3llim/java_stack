d880417ab34540879e0f8249b252b253306decd3
ma3llim <mohdsameer68257@gmail.com>
2026-03-01 12:58:28 +0530

    update the folder strcuture

---
+++ 03_advJava/TryCatch.java
@@ -0,0 +1,14 @@
+public class TryCatch {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        try {
+            j = 18 / i;
+        } catch (Exception e) {
+            System.out.println("Somethings Went Wrong...");
+        }
+
+        System.out.println(j);
+        System.out.println("Bye");
+    }
+}
