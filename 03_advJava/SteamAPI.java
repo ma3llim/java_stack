88636a1ac2c0a8ac68f309c520e8d883665ab1e1
ma3llim <mohdsameer68257@gmail.com>
2026-03-03 20:48:22 +0530

    chore: uncomment wanted code

--- 03_advJava/SteamAPI.java
+++ 03_advJava/SteamAPI.java
@@ -9,7 +9,7 @@ public class SteamAPI {
         Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
         Stream<Integer> s3 = s2.map(n -> n * 2);
         int result = s3.reduce(0, (c, e) -> c + e);
-        // System.out.println(result);
+        System.out.println(result);
 
         List<Integer> numbers = Arrays.asList(4, 5, 7, 2, 8);
         int result2 = numbers.stream().filter(n -> n % 2 == 0).map(n -> n * 2).reduce(0, (c, e) -> c + e);
