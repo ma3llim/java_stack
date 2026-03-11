b707998fb467536ca762b99cfb37e022f10d0e9d
ma3llim <mohdsameer68257@gmail.com>
2026-03-11 20:50:28 +0530

    chore: learn bean object creation

--- 07_spring/configure_spring/src/main/java/org/example/Laptop.java
+++ 07_spring/configure_spring/src/main/java/org/example/Laptop.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Laptop {
+
+    public Laptop(){
+        System.out.println("Object Created For Laptop");
+    }
+
     public void device() {
         System.out.println("Device");
     }
--- 07_spring/configure_spring/src/main/java/org/example/Student.java
+++ 07_spring/configure_spring/src/main/java/org/example/Student.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Student {
+    public Student(){
+        System.out.println("Object Created For Student");
+    }
+
     public void code(){
         System.out.println("Code");
     }
