83d7aa5b275dc4dbde887a7ad24fe4936d7f60b8
ma3llim <mohdsameer68257@gmail.com>
2026-03-12 21:10:31 +0530

    chore: learn getBrean by type

--- 07_spring/configure_spring/src/main/java/org/example/Main.java
+++ 07_spring/configure_spring/src/main/java/org/example/Main.java
@@ -6,8 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args){
         ApplicationContext context =new ClassPathXmlApplicationContext("spring.xml");
-        Student obj = (Student) context.getBean("student");
+        Student obj = (Student) context.getBean("student", Student.class);
         // Laptop obj2 = (Laptop) context.getBean("laptop");
-        obj.code();
+        // obj.code();
+        Desktop desktopObj = context.getBean(Desktop.class);
+
     }
 }
