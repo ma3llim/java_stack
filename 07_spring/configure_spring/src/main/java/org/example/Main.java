7072d389322883cbf700c8ea5be5e213fff10a0b
ma3llim <mohdsameer68257@gmail.com>
2026-03-12 20:53:08 +0530

    chore: learn the autowiring

--- 07_spring/configure_spring/src/main/java/org/example/Main.java
+++ 07_spring/configure_spring/src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args){
         ApplicationContext context =new ClassPathXmlApplicationContext("spring.xml");
         Student obj = (Student) context.getBean("student");
-        Laptop obj2 = (Laptop) context.getBean("laptop");
-        System.out.println(obj.getName());
+        // Laptop obj2 = (Laptop) context.getBean("laptop");
+        obj.code();
     }
 }
