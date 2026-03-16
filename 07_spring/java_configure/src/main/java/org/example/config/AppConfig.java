6b7fad20ed82f908e4abe172700d0d92f5111e9a
ma3llim <mohdsameer68257@gmail.com>
2026-03-16 21:39:50 +0530

    chore: learn the qualifier in java based configuration

--- 07_spring/java_configure/src/main/java/org/example/config/AppConfig.java
+++ 07_spring/java_configure/src/main/java/org/example/config/AppConfig.java
@@ -1,13 +1,15 @@
 package org.example.config;
 import org.example.Computer;
 import org.example.Desktop;
+import org.example.Laptop;
 import org.example.Student;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 
 public class AppConfig {
 
     @Bean()
-    public Student student(Computer com){
+    public Student student(@Qualifier("desktop") Computer com){
         Student obj = new Student();
         obj.setName("Mohd Sameer");
         obj.setCom(com);
@@ -17,5 +19,10 @@ public class AppConfig {
     @Bean()
     public Desktop desktop(){
         return new Desktop();
+    }
+
+    @Bean
+    public Laptop laptop(){
+        return new Laptop();
     }
 }
