dc77862b5a067e86d2814711c1a15c850d31c51a
ma3llim <mohdsameer68257@gmail.com>
2026-03-16 22:09:53 +0530

    chore: learn the scope and value annotation in java based configuration

--- 07_spring/java_configure/src/main/java/org/example/Student.java
+++ 07_spring/java_configure/src/main/java/org/example/Student.java
@@ -2,14 +2,15 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.beans.ConstructorProperties;
 
 @Component
-@Primary
 public class Student {
+    @Value("Mohd Sameer")
     private String name;
     private Computer com;
 
