3bcd85b192fdc71565bf748dd6c4ff500f042306
ma3llim <mohdsameer68257@gmail.com>
2026-03-19 22:00:16 +0530

    chore: learn the JDBC template

--- 08_spring_jdbc/jdbc_h2/src/main/java/com/example/jdbc_h2/Repositories/StudentRepo.java
+++ 08_spring_jdbc/jdbc_h2/src/main/java/com/example/jdbc_h2/Repositories/StudentRepo.java
@@ -1,6 +1,8 @@
 package com.example.jdbc_h2.Repositories;
 
 import com.example.jdbc_h2.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,8 +10,21 @@ import java.util.List;
 
 @Repository
 public class StudentRepo {
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void save(Student s){
-       System.out.println(s);
+        String sql = "insert into student (rollno, name, marks) values (?,?,?)";
+        int rows = jdbcTemplate.update(sql, s.getRollNo(), s.getName(), s.getMarks());
+        System.out.println(rows);
     }
 
     public List<Student> findAll() {
