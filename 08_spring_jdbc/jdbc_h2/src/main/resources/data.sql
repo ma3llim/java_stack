e33994b3c7561c37d8dbb83d0625108ca99d1db8
ma3llim <mohdsameer68257@gmail.com>
2026-03-19 22:36:22 +0530

    chore: learn how to do Migration from h2 -> postgresql

--- 08_spring_jdbc/jdbc_h2/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into student (rollno, name, marks) values (1,'Zainab',99);
-insert into student (rollno, name, marks) values (2,'Fahad',97);
-insert into student (rollno, name, marks) values (3,'Salman',99);
--- 08_spring_jdbc/jdbc_h2/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-create table student (
-    rollno INT primary key,
-    name VARCHAR(50),
-    marks INT
-);
