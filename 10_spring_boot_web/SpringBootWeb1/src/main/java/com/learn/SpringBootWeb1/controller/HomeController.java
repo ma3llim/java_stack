550b5ee0f957b092f17fcc25d290763a9d12bc1b
ma3llim <mohdsameer68257@gmail.com>
2026-03-23 12:22:08 +0530

    chore: remove sessions and move to model

--- 10_spring_boot_web/SpringBootWeb1/src/main/java/com/learn/SpringBootWeb1/controller/HomeController.java
+++ 10_spring_boot_web/SpringBootWeb1/src/main/java/com/learn/SpringBootWeb1/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.learn.SpringBootWeb1.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,9 +16,9 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int number1, @RequestParam("num2") int number2, HttpSession session){
+    public String add(@RequestParam("num1") int number1, @RequestParam("num2") int number2, Model model){
         int result = number1+number2;
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
         return "result.jsp";
     }
 }
