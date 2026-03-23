b8036d5007127529e429de3585c823758c92f20d
ma3llim <mohdsameer68257@gmail.com>
2026-03-23 22:52:39 +0530

    chore: update the folder structure

---
+++ 11_projects/01_job_project/src/main/java/com/sameer/JobApp/controllers/JobController.java
@@ -0,0 +1,12 @@
+package com.sameer.JobApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+    @RequestMapping({"/", "home"})
+    public String home(){
+        return "home";
+    }
+}
