7fc150c9cf9a1f85184586a72ec8c4a81f43e246
ma3llim <mohdsameer68257@gmail.com>
2026-03-23 12:17:51 +0530

    chore: now using the requestParam with spring argument

--- 10_spring_boot_web/SpringBootWeb1/src/main/java/com/learn/SpringBootWeb1/controller/HomeController.java
+++ 10_spring_boot_web/SpringBootWeb1/src/main/java/com/learn/SpringBootWeb1/controller/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -14,8 +15,8 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(int num1, int num2, HttpSession session){
-        int result = num1+num2;
+    public String add(@RequestParam("num1") int number1, @RequestParam("num2") int number2, HttpSession session){
+        int result = number1+number2;
         session.setAttribute("result", result);
         return "result.jsp";
     }
