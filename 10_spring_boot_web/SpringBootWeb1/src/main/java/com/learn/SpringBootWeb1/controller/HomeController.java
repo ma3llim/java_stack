f45914d80df978c98868001246c3240ece88359c
ma3llim <mohdsameer68257@gmail.com>
2026-03-23 12:38:31 +0530

    chore: update the model to modelAndView

--- 10_spring_boot_web/SpringBootWeb1/src/main/java/com/learn/SpringBootWeb1/controller/HomeController.java
+++ 10_spring_boot_web/SpringBootWeb1/src/main/java/com/learn/SpringBootWeb1/controller/HomeController.java
@@ -1,11 +1,9 @@
 package com.learn.SpringBootWeb1.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -16,9 +14,11 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int number1, @RequestParam("num2") int number2, Model model){
-        int result = number1+number2;
-        model.addAttribute("result", result);
-        return "result";
+    public ModelAndView add(@RequestParam("num1") int number1, @RequestParam("num2") int number2, ModelAndView modelView){
+        int result = number1+number2;0
+
+        modelView.addObject("result", result);
+        modelView.setViewName("result");
+        return modelView;
     }
 }
