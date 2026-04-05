2b046a3880e1bad0f7b0c4ded59a63bfbb66bcd2
ma3llim <mohdsameer68257@gmail.com>
2026-04-05 23:07:41 +0530

    chore: add view data all jobs listing

--- 11_projects/01_job_project/src/main/java/com/sameer/JobApp/controllers/JobController.java
+++ 11_projects/01_job_project/src/main/java/com/sameer/JobApp/controllers/JobController.java
@@ -4,8 +4,11 @@ import com.sameer.JobApp.model.JobPost;
 import com.sameer.JobApp.repositories.JobRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -26,5 +29,12 @@ public class JobController {
     public String handleForm(JobPost jobPost){
         repo.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model m){
+        List<JobPost> jobs = repo.getAllJobs();
+        m.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
     }
 }
