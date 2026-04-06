d97725c05d24a5a3d350326c415be6d9a3028e1e
ma3llim <mohdsameer68257@gmail.com>
2026-04-06 23:25:24 +0530

    chore: added the cross-origin to connect with frontend

--- 11_projects/2_spring_rest/src/main/java/com/sameer/spring_boot_rest/JobControlller.java
+++ 11_projects/2_spring_rest/src/main/java/com/sameer/spring_boot_rest/JobControlller.java
@@ -4,12 +4,14 @@ import com.sameer.spring_boot_rest.model.JobPost;
 import com.sameer.spring_boot_rest.services.JobServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobControlller {
     @Autowired
     private JobServices services;
