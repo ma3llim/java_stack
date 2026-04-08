8f0261778154407f3332233caf67b7f0324ea3fb
ma3llim <mohdsameer68257@gmail.com>
2026-04-08 23:13:12 +0530

    convert the the controller into Restcontroller

--- 11_projects/2_spring_rest/src/main/java/com/sameer/spring_boot_rest/JobController.java
+++ 11_projects/2_spring_rest/src/main/java/com/sameer/spring_boot_rest/JobController.java
@@ -3,31 +3,27 @@ package com.sameer.spring_boot_rest;
 import com.sameer.spring_boot_rest.model.JobPost;
 import com.sameer.spring_boot_rest.services.JobServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class JobController {
     @Autowired
     private JobServices services;
 
     @GetMapping("/jobPosts")
-    @ResponseBody
     public List<JobPost> getAllJobs(){
         return services.getAllJobs();
     }
 
     @GetMapping("/jobdetails/{postId}")
-    @ResponseBody
     public JobPost getJob(@PathVariable int postId){
         return services.getJob(postId);
     }
 
     @PostMapping("/jobPost")
-    @ResponseBody
     public void addJob(@RequestBody JobPost jobPost){
         services.addJob(jobPost);
     }
