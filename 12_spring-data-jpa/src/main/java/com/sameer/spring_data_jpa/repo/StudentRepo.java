a3a5f6255d13f12e0a3fc390c875eef7221055bc
ma3llim <mohdsameer68257@gmail.com>
2026-04-12 21:57:34 +0530

    chore: few more pre-define methods created by the jpql query

--- 12_spring-data-jpa/src/main/java/com/sameer/spring_data_jpa/repo/StudentRepo.java
+++ 12_spring-data-jpa/src/main/java/com/sameer/spring_data_jpa/repo/StudentRepo.java
@@ -2,7 +2,6 @@ package com.sameer.spring_data_jpa.repo;
 
 import com.sameer.spring_data_jpa.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +11,7 @@ public interface StudentRepo extends JpaRepository<Student, Integer> {
 
     // @Query("select s from Student s where s.name = ?1") -- This is created by the JPA
     List<Student>findByName(String name);
+    List<Student>findByMarks(int marks);
+    List<Student>findByRollNo(int roll);
+    List<Student>findByMarksGreaterThan(int roll);
 }
