33d8e9a5a3938ca288307e3ba7a34987f44f3138
ma3llim <mohdsameer68257@gmail.com>
2026-04-19 19:58:34 +0530

    chore: added responseEntity in product api response

--- 11_projects/3_ecommerce/src/main/java/com/sameer/ecommerce/controllers/ProductController.java
+++ 11_projects/3_ecommerce/src/main/java/com/sameer/ecommerce/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.sameer.ecommerce.controllers;
 import com.sameer.ecommerce.model.Product;
 import com.sameer.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public List<Product> getProducts(){
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
 }
