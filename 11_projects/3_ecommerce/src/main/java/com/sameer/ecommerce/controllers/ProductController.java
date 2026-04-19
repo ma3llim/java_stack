c4afc8746a24ee19b59f31fca3a547a43fb00e7c
ma3llim <mohdsameer68257@gmail.com>
2026-04-19 23:12:40 +0530

    chore: add product route with image

--- 11_projects/3_ecommerce/src/main/java/com/sameer/ecommerce/controllers/ProductController.java
+++ 11_projects/3_ecommerce/src/main/java/com/sameer/ecommerce/controllers/ProductController.java
@@ -4,12 +4,13 @@ import com.sameer.ecommerce.model.Product;
 import com.sameer.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,8 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductService productService;
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @GetMapping
     public ResponseEntity<List<Product>> getProducts(){
         return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
@@ -33,4 +35,15 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @PostMapping(value ="product", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> addProduct(@RequestPart("product") String productJson, @RequestPart("imageFile") MultipartFile imageFile){
+        try {
+            Product product = objectMapper.readValue(productJson, Product.class);
+            Product saveProduct = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(saveProduct, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    };
 }
--- 11_projects/3_ecommerce/src/main/java/com/sameer/ecommerce/model/Product.java
+++ 11_projects/3_ecommerce/src/main/java/com/sameer/ecommerce/model/Product.java
@@ -1,10 +1,7 @@
 package com.sameer.ecommerce.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +16,7 @@ import java.util.Date;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String description;
     private String brand;
@@ -29,4 +26,8 @@ public class Product {
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
 }
