c215d5c488d6f9e37c5dfd56918478a2a0e864de
ma3llim <mohdsameer68257@gmail.com>
2026-04-22 23:45:41 +0530

    chore: updated the table for testing the data

--- 13_spring-boot-rest/src/main/java/com/sameer/spring_data_rest/repo/JobRepo.java
+++ 13_spring-boot-rest/src/main/java/com/sameer/spring_data_rest/repo/JobRepo.java
@@ -1,5 +1,7 @@
 package com.sameer.spring_data_rest.repo;
 
+import com.sameer.spring_data_rest.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
