97cc6abb25739d7272e976fee8c510ba60aa034b
ma3llim <mohdsameer68257@gmail.com>
2026-04-22 23:14:19 +0530

    chore: created get all order route

--- 11_projects/3_ecommerce/src/main/java/com/sameer/ecommerce/service/OrderServices.java
+++ 11_projects/3_ecommerce/src/main/java/com/sameer/ecommerce/service/OrderServices.java
@@ -76,6 +76,32 @@ public class OrderServices {
     }
 
     public List<OrderResponse> getAllOrders() {
-        return null;
+        List<Order> orders = orderRepo.findAll();
+        List<OrderResponse> orderResponses = new ArrayList<>();
+
+        for(Order order: orders){
+            List<OrderItemResponse> itemResponses = new ArrayList<>();
+
+            for(OrderItem item : order.getOrderItems()){
+                OrderItemResponse orderItemResponse = new OrderItemResponse(
+                        item.getProduct().getName(),
+                        item.getQuantity(),
+                        item.getTotalPrice()
+                );
+                itemResponses.add(orderItemResponse);
+            }
+
+            OrderResponse orderResponse = new OrderResponse(
+                    order.getOrderId(),
+                    order.getCustomerName(),
+                    order.getEmail(),
+                    order.getStatus(),
+                    order.getOrderDate(),
+                    itemResponses
+            );
+            orderResponses.add(orderResponse);
+        }
+
+        return orderResponses;
     }
 }
