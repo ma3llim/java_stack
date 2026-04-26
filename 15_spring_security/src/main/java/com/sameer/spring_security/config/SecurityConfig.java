6e9a471273a7adb5a0f5e40dcb1c8461a9fa5f5d
Mohd Sameer <mohdsameer68257@gmail.com>
2026-04-26 23:53:47 +0530

    chore: loading users data and from userDetailsServices with inMemory

--- 15_spring_security/src/main/java/com/sameer/spring_security/config/SecurityConfig.java
+++ 15_spring_security/src/main/java/com/sameer/spring_security/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +18,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-        httpSecurity.csrf(customizer -> customizer.disable();
+        httpSecurity.csrf(customizer -> customizer.disable());
         httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
         httpSecurity.httpBasic(Customizer.withDefaults());
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return  httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user  = User.withDefaultPasswordEncoder().username("user").password("root").roles("admin").build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
