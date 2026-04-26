8128959f4c4394864f3ff51d09a1b97871cc00c1
Mohd <mohdsameer68257@gmail.com>
2026-04-26 21:15:04 +0530

    chore: create student controller

--- 15_spring_security/src/main/java/com/sameer/spring_security/controller/HelloController.java
+++ 15_spring_security/src/main/java/com/sameer/spring_security/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.sameer.spring_security;
+package com.sameer.spring_security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ 15_spring_security/src/main/java/com/sameer/spring_security/controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.sameer.spring_security.controller;
+
+import com.sameer.spring_security.model.Student;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+    List<Student> = new ArrayList<>(List.of(
+            
+            )
+}
