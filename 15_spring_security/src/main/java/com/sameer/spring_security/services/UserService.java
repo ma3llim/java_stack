4b0974010d23fd06dbc63304cac462fd6e4032dc
Mohd Sameer <mohdsameer68257@gmail.com>
2026-04-28 00:23:51 +0530

    chore: bcrypt encoding for user registration

--- 15_spring_security/src/main/java/com/sameer/spring_security/services/UserService.java
+++ 15_spring_security/src/main/java/com/sameer/spring_security/services/UserService.java
@@ -3,11 +3,15 @@ package com.sameer.spring_security.services;
 import com.sameer.spring_security.model.User;
 import com.sameer.spring_security.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+
     public User saveUser(User user){
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 }
