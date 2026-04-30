1d8c703d60d210d0e8ec281e6faf5140a21dcc55
Mohd Sameer <mohdsameer68257@gmail.com>
2026-04-30 23:39:50 +0530

    chore: initialize spring OAuth Project

---
+++ 17_spring_oauth/src/test/java/com/sameer/springOAuth/SpringOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameer.springOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
