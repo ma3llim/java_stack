38758bd8a2a72bc2ee7e0c66a0469607619bb921
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-03 20:51:12 +0530

    chore: initinalize the spring ai project

---
+++ 19_spring_ai/01_spring_ai_learn/src/main/java/com/sameer/spring_ai_learn/SpringAiLearnApplication.java
@@ -0,0 +1,12 @@
+package com.sameer.spring_ai_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiLearnApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiLearnApplication.class, args);
+	}
+
+}
---
+++ 19_spring_ai/01_spring_ai_learn/src/test/java/com/sameer/spring_ai_learn/SpringAiLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameer.spring_ai_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
