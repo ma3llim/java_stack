0121c323592b1ae822fd2c40b455053327caf695
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-19 23:57:24 +0530

    chore: created student class

---
+++ 01_basic/StudentsProgram.java
@@ -0,0 +1,25 @@
+class StudentClass {
+    private final String name;
+    private final int age;
+    private final char grade;
+
+    StudentClass(String name, int age, char grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    void display() {
+        System.out.println("Name  : " + name);
+        System.out.println("Age   : " + age);
+        System.out.println("Grade : " + grade);
+        System.out.println();
+    }
+}
+
+public class StudentsProgram {
+    public static void main(String[] args){
+        StudentClass studentClass1 = new StudentClass("John", 90, 'A');
+        studentClass1.display();
+    }
+}
