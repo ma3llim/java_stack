a5fd74ec02592a283508f3e902bab6323cf20ed1
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-20 23:48:11 +0530

    chore: created the multplication of the table program

--- 01_basic/challenges/basic/MultiplicationTable.java
+++ 01_basic/challenges/basic/MultiplicationTable.java
@@ -1,4 +1,10 @@
 package challenges.basic;
 
 public class MultiplicationTable {
+    public static void main(String[] args){
+        int tableNumber = 4;
+        for (int i = 1; i < 11; i++){
+            System.out.println(tableNumber + " x " + i + " = " + (i*tableNumber));
+        }
+    }
 }
