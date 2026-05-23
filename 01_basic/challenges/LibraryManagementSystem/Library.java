afb949a4f19f79e85bd519360f2de7ef5e5b3dd2
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-23 23:56:03 +0530

    chore: created the method in library class libraryBooks

--- 01_basic/challenges/LibraryManagementSystem/Library.java
+++ 01_basic/challenges/LibraryManagementSystem/Library.java
@@ -35,9 +35,20 @@ public class Library {
         return booksSearchByAuthor;
     }
 
+    ArrayList<Book> libraryBooks(String author){
+        return books;
+    }
+
     public static void main(String[] args){
         Book cleanCode = new Book("Clean Code", "Robert Martin", "1", true);
         Book effectiveJava = new Book("Effective Java", "Joshua Bloch", "2", false);
         Book headFirstJava = new Book("Head First Java", "Kathy Sierra", "3", true);
+        Library libraryStored = new Library();
+
+        libraryStored.addBook(cleanCode);
+        libraryStored.addBook(effectiveJava);
+        libraryStored.addBook(headFirstJava);
+
+
     }
 }
