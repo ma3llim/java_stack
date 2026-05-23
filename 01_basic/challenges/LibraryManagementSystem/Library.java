c9e5201f11de78f77af33e41ca0d170a743e3c82
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-23 23:46:36 +0530

    chore: created the methods in library class of addBook, removeBook, searchByTitle

--- 01_basic/challenges/LibraryManagementSystem/Library.java
+++ 01_basic/challenges/LibraryManagementSystem/Library.java
@@ -1,11 +1,27 @@
 package challenges.LibraryManagementSystem;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    private List<Book> books;
+    private ArrayList<Book> books = new ArrayList<>();
 
     void addBook(Book book){
+        books.add(book);
+    }
 
+    void removeBook(Book book){
+        books.remove(book);
+    }
+
+    ArrayList<Book> searchByTitle(String title){
+        ArrayList<Book> booksSearchByTitle = new ArrayList<>();
+
+        for(Book book: books){
+            if(book.equals(title)){
+                booksSearchByTitle.add(book);
+            }
+        }
+        return booksSearchByTitle;
     }
 
     public static void main(String[] args){
