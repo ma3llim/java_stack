1c8fbd799a1d7b6a0f61e69cb4d62456c7628047
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-24 21:43:50 +0530

    chore: created shapeHierarchy with shape class

---
+++ 01_basic/ShapeHierarchy/Shape.java
@@ -0,0 +1,20 @@
+package ShapeHierarchy;
+
+public class Shape {
+    String color;
+
+    Shape(String color){
+        this.color = color;
+    }
+
+    String getColor(){
+        return this.color;
+    }
+
+    double area(){
+        return 0;
+    }
+    double perimeter(){
+        return 0;
+    }
+}
