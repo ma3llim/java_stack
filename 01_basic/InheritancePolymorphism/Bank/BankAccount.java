93767fcd9af2b20aaef4c6a16c7065355d5bd3d8
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-25 10:20:00 +0530

    feat: rewrite BankAccount using full encapsulation

--- 01_basic/InheritancePolymorphism/Bank/BankAccount.java
+++ 01_basic/InheritancePolymorphism/Bank/BankAccount.java
@@ -1,7 +1,31 @@
 package InheritancePolymorphism.Bank;
 
 public class BankAccount {
+    private String owner;
     private int balance;
+
+    BankAccount(String owner, int balance){
+        this.owner = owner;
+
+        if (balance >= 0) {
+            this.balance = balance;
+        } else {
+            this.balance = 0;
+            System.out.println("Invalid balance. Set to 0");
+        }
+    }
+
+    public String getOwner(){
+        return owner;
+    }
+
+    public void SetOwner(String owner){
+        if(!owner.isEmpty()){
+            this.owner = owner;
+        } else {
+            System.out.println("Owner name cannot be empty");
+        }
+    }
 
     public void setBalance(int balance) {
 
@@ -19,7 +43,7 @@ public class BankAccount {
 
     public static void main(String[] args) {
 
-        BankAccount account = new BankAccount();
+        BankAccount account = new BankAccount("Sameer", 1000);
 
         account.setBalance(1000);
         System.out.println(account.getBalance());
