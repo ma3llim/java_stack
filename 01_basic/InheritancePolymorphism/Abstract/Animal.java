5c6c27ae3ff17075f8ac0a11fbc65a814803e37d
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-26 10:20:00 +0530

    feat: implement abstraction using abstract classes and interfaces

--- 01_basic/InheritancePolymorphism/Abstract/Animal.java
+++ 01_basic/InheritancePolymorphism/Abstract/Animal.java
@@ -1,4 +1,49 @@
 package InheritancePolymorphism.Abstract;
 
+abstract class AnimalAbstract {
+    protected String name;
+
+    AnimalAbstract(String name){
+        this.name = name;
+    }
+
+    void eat(){
+        System.out.println(name + " is eating");
+    }
+
+    abstract void makeSound();
+}
+
+class Dog extends AnimalAbstract {
+    Dog(String name){
+        super(name);
+    }
+
+    @Override
+    void makeSound(){
+        System.out.println("Bark");
+    }
+}
+
+class Cat extends AnimalAbstract {
+    Cat(String name){
+        super(name);
+    }
+
+    @Override
+    void makeSound(){
+        System.out.println("Meow");
+    }
+}
+
 public class Animal {
+    static void main(String[] args) {
+        AnimalAbstract dog = new Dog("Tommy");
+        AnimalAbstract cat = new Cat("Kitty");
+
+        dog.eat();
+        dog.makeSound();
+        cat.eat();
+        cat.makeSound();
+    }
 }
--- 01_basic/InheritancePolymorphism/Abstract/Interfaces.java
+++ 01_basic/InheritancePolymorphism/Abstract/Interfaces.java
@@ -1,4 +1,29 @@
 package InheritancePolymorphism.Abstract;
 
+interface Payable {
+    void pay();
+}
+
+class Employee implements Payable {
+    @Override
+    public void pay(){
+        System.out.println("Employee salary paid");
+    }
+}
+
+class Contractor implements Payable {
+    @Override
+    public void pay(){
+        System.out.println("Contractor payment paid");
+    }
+}
+
 public class Interfaces {
+    static void main(String[] args) {
+        Payable employee = new Employee();
+        Payable contractor = new Contractor();
+
+        employee.pay();
+        contractor.pay();
+    }
 }
