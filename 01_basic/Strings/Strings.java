2832708edc8ff625ee03a6e6e65d1c9384bbd48c
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-29 18:32:59 +0530

    feat: implement string immutability examples

---
+++ 01_basic/Strings/Strings.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Strings {
+    static void main(String[] args) {
+        String a = "hello";
+        String b = "World";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
