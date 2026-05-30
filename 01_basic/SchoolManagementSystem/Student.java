d99a571c6209ddb9d0a1ee0bf445353699fb5ed1
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-30 23:47:08 +0530

    feat: created the inheritance student class

--- 01_basic/SchoolManagementSystem/Student.java
+++ 01_basic/SchoolManagementSystem/Student.java
@@ -1,10 +1,36 @@
 package SchoolManagementSystem;
 
-public abstract class Student {
-    protected int id;
-    protected String name;
-    protected int age;
-    protected String grade;
-    protected String enrollmentDate;
-    protected String studentId;
+import java.util.Objects;
+
+public class Student extends Person implements Gradable {
+    private Grade grade;
+    private String studentId;
+
+
+    @Override
+    public Grade getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "grade=" + grade +
+                ", studentId='" + studentId + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) return  true;
+        if(!(obj instanceof Student)) return false;
+        if(!super.equals(obj)) return  false;
+        Student student = (Student) obj;
+        return Objects.equals(studentId, student.studentId);
+    }
+
+    @Override
+    public int hashCode(){
+        return  Objects.hash(super.hashCode(), studentId, grade);
+    }
 }
