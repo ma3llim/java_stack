b383b9ac7f88a86f24860a6b1347be6d9563a3bd
Mohd Sameer <mohdsameer68257@gmail.com>
2026-05-31 22:17:38 +0530

    feat: add InsufficientFundsException exception class

---
+++ 01_basic/ExecptionHandling/exceptions/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package ExecptionHandling.exceptions;
+
+public class InsufficientFundsException extends Exception{
+    public InsufficientFundsException(String message){
+        super(message);
+    }
+}
