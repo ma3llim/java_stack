33813ec1baa4a5cf02377dfd208c5fa811866cdc
Mohd Sameer <mohdsameer68257@gmail.com>
2026-06-03 10:20:00 +0530

    feat: created ATM Package and complete the print statement interface

---
+++ 01_basic/ATM/PrintStatement.java
@@ -0,0 +1,31 @@
+package ATM;
+
+interface Printable {
+    void printStatement();
+}
+
+public class PrintStatement implements Printable {
+    private String name;
+    private String bankNumber;
+    private String accountType;
+    private double balance;
+
+    public PrintStatement(String name, String bankNumber, String accountType, double balance){
+        this.name = name;
+        this.bankNumber = bankNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    @Override
+    public void printStatement(){
+        System.out.println("======================================");
+        System.out.println("            ATM STATEMENT             ");
+        System.out.println("======================================");
+        System.out.printf("Name         : %s%n", name);
+        System.out.printf("Bank Number  : %s%n", bankNumber);
+        System.out.printf("Account Type : %s%n", accountType);
+        System.out.printf("Balance      : %.2f%n", balance);
+        System.out.println("======================================");
+    }
+}
