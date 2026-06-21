920fecf9aa40d373a1ca440dee46927dfaecec37
Mohd Sameer <mohdsameer68257@gmail.com>
2026-06-21 11:32:56 +0530

    chore: learn the generic

---
+++ 03_advJava/CollectionsPrograms/GenericExample.java
@@ -0,0 +1,20 @@
+package CollectionsPrograms;
+
+class Box<T> {
+    private T value;
+
+    public void set(T value){
+        this.value = value;
+    }
+
+    public T get(){
+        return value;
+    }
+}
+public class GenericExample {
+    public static void main(String[] args){
+        Box<String> box = new Box<>();
+        box.set("Java");
+        System.out.println(box.get());
+    }
+}
