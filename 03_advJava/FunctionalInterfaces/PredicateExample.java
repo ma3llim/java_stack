2344dc68f44533739aafc14c943c6fccb83b823b
Mohd Sameer <mohdsameer68257@gmail.com>
2026-06-22 23:03:58 +0530

    chore: learn the predicate functional interfaces

---
+++ 03_advJava/FunctionalInterfaces/PredicateExample.java
@@ -0,0 +1,11 @@
+package FunctionalInterfaces;
+
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    public static void main(String[] args){
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+        System.out.println(isEven.test(7));
+        System.out.println(isEven.test(6));
+    }
+}
