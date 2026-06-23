3d9a8af2619bf3c47b5ef0254e3f3c8991a51af6
Mohd Sameer <mohdsameer68257@gmail.com>
2026-06-24 00:04:48 +0530

    feat: solved two sum with brute force

---
+++ 21_leetcode/easy/TwoSum.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.Arrays;
+
+public class TwoSum {
+   public int[] twoSumBruteForce(int[] nums, int target){
+       for(int i = 0; i < nums.length; i++){
+           for(int j = i + 1; j < nums.length; j++){
+               if(nums[i] + nums[j] == target){
+                   return new int[]{i, j};
+               }
+           }
+       }
+       return new int[]{};
+   }
+
+    public static void main(String[] args){
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        TwoSum twoSum = new TwoSum();
+
+        System.out.println(Arrays.toString(twoSum.twoSumBruteForce(nums, target)));
+    }
+}
