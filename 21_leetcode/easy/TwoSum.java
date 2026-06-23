e57bf9f35a187cdf1465c123e8a89c103dc88f28
Mohd Sameer <mohdsameer68257@gmail.com>
2026-06-24 00:22:51 +0530

    feat: solved two sum with optimal approach

--- 21_leetcode/easy/TwoSum.java
+++ 21_leetcode/easy/TwoSum.java
@@ -1,6 +1,7 @@
 package easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
    public int[] twoSumBruteForce(int[] nums, int target){
@@ -14,11 +15,23 @@ public class TwoSum {
        return new int[]{};
    }
 
+   public int[] twoSumOptimal(int[] nums, int target){
+       HashMap<Integer, Integer> hashMap = new HashMap<>();
+       for(int i = 0; i < nums.length; i++){
+           int complient = target - nums[i];
+           if(hashMap.containsKey(complient)){
+               return new int[]{hashMap.get(complient), i};
+           }
+           hashMap.put(nums[i],i);
+       }
+       return new int[]{};
+   }
+
     public static void main(String[] args){
         int[] nums = {2,7,11,15};
         int target = 9;
         TwoSum twoSum = new TwoSum();
 
-        System.out.println(Arrays.toString(twoSum.twoSumBruteForce(nums, target)));
+        System.out.println(Arrays.toString(twoSum.twoSumOptimal(nums, target)));
     }
 }
