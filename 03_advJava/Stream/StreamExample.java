4a270241954a034697db3c6f8b2e6351fb0bc531
Mohd Sameer <mohdsameer68257@gmail.com>
2026-06-26 17:27:12 +0530

    feat: learn steam methods of sorted, distinct, limit, skip

--- 03_advJava/Stream/StreamExample.java
+++ 03_advJava/Stream/StreamExample.java
@@ -9,21 +9,34 @@ public class StreamExample {
     public static void main(String[] args){
         // filter
         List<Integer> nums = List.of(1,2,3,4,5,6,7,8,9);
-        nums.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+        // nums.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
 
         // map
         List<String> names = List.of("sameer", "rahul");
-        names.stream().map(String::toUpperCase).forEach(System.out::println);
+        // names.stream().map(String::toUpperCase).forEach(System.out::println);
 
         // reduce
         int sum = List.of(1,2,3,4,5).stream().reduce(0, (a,b)-> a+b);
-        System.out.println(sum);
+        // System.out.println(sum);
 
         // collect
         List<String> result = names.stream().filter(name-> name.length() > 4).collect(Collectors.toList());
-        System.out.println(result);
+        // System.out.println(result);
 
         // forEach
-        names.forEach(System.out::println);
+        // names.forEach(System.out::println);
+
+        // sorted
+        List<Integer> numbers = List.of(5, 2, 8, 1, 1, 2);
+        // numbers.stream().sorted().forEach(System.out::println);
+
+        // distinct
+        // numbers.stream().distinct().forEach(System.out::println);
+
+        // Limit
+        // numbers.stream().limit(3).forEach(System.out::println);
+
+        // skip
+        numbers.stream().skip(2).forEach(System.out::println);
     }
 }
