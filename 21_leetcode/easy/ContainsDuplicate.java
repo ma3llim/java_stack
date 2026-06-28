46456b24016cd54e331d6863056d3871e0321364
Mohd Sameer <mohdsameer68257@gmail.com>
2026-06-28 20:15:47 +0530

    feat: solved contains duplicate problem with better  approach

--- 21_leetcode/easy/ContainsDuplicate.java
+++ 21_leetcode/easy/ContainsDuplicate.java
@@ -12,10 +12,16 @@ public class ContainsDuplicate {
         }
         return false;
     }
-
+    public boolean containsDuplicateBetterAppraoch(int[] nums){
+        Arrays.sort(nums);
+        for(int i=0; i < nums.length-1; i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return false;
+    }
     public static void main(String[] args){
         ContainsDuplicate containsDuplicate = new ContainsDuplicate();
-        System.out.println(containsDuplicate.containsDuplicateBruteForce(new int[]{1, 2, 3, 1}));
-        System.out.println(containsDuplicate.containsDuplicateBruteForce(new int[]{1, 2, 3, 4}));
+        System.out.println(containsDuplicate.containsDuplicateBetterAppraoch(new int[]{1, 2, 3, 1}));
+        System.out.println(containsDuplicate.containsDuplicateBetterAppraoch(new int[]{1, 2, 3, 4}));
     }
 }
