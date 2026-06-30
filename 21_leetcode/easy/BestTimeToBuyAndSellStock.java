3dd79cd6cab3728fc0375e2b78572f0e30b4bb1c
Mohd Sameer <mohdsameer68257@gmail.com>
2026-06-30 22:44:28 +0530

    feat: solved best time to buy and sell stock with better approach

--- 21_leetcode/easy/BestTimeToBuyAndSellStock.java
+++ 21_leetcode/easy/BestTimeToBuyAndSellStock.java
@@ -11,12 +11,21 @@ public class BestTimeToBuyAndSellStock {
 
         return res;
     }
+    public int maxProfileBetterApproach(int[] prices){
+        int minSoFar = prices[0];
+        int res = 0;
+        for(int i = 0; i < prices.length-1; i++){
+            minSoFar = Math.min(minSoFar, prices[i]);
+            res = Math.max(res, prices[i] - minSoFar);
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         int[] prices = new int[]{7, 1, 5, 3, 6, 4};
         int[] prices2 = new int[]{7, 6, 4, 3, 1};
         BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
-        System.out.println(bestTimeToBuyAndSellStock.maxProfitBruteForce(prices));
-        System.out.println(bestTimeToBuyAndSellStock.maxProfitBruteForce(prices2));
+        System.out.println(bestTimeToBuyAndSellStock.maxProfileBetterApproach(prices));
+        System.out.println(bestTimeToBuyAndSellStock.maxProfileBetterApproach(prices2));
     }
 }
