3f9c17b959ff7587a4a72d1df5834748a423cf80
Mohd Sameer <mohdsameer68257@gmail.com>
2026-07-01 00:27:39 +0530

    feat: solved maximum sub array with most optimal approach

--- 21_leetcode/easy/MaximumSubarray.java
+++ 21_leetcode/easy/MaximumSubarray.java
@@ -31,6 +31,18 @@ public class MaximumSubarray {
         return maxSum;
     }
 
+    public int maxSubArrayOptimalForce(int[] nums) {
+        int maxSum = Integer.MIN_VALUE, currentSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            maxSum = Math.max(currentSum, maxSum);
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+
     public static void main(String[] args) {
         MaximumSubarray maximumSubarray = new MaximumSubarray();
         System.out.println(maximumSubarray.maxSubArrayBetterForce(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
