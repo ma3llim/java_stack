3da3aef63cb6cd320a8e9d2c78a6b241cfc55b78
Mohd Sameer <mohdsameer68257@gmail.com>
2026-07-04 00:10:44 +0530

    feat: solved reverse string with better approach

--- 21_leetcode/easy/ReverseString.java
+++ 21_leetcode/easy/ReverseString.java
@@ -10,8 +10,21 @@ public class ReverseString {
         }
     }
 
+    public void reverseStringBetter(char[] s) {
+        char[] result = new char[s.length];
+        int j = 0;
+        
+        for (int i = s.length - 1; i >= 0; i--) {
+            result[j] = s[i];
+            j++;
+        }
+        for (int i = 0; i < s.length; i++) {
+            s[i] = result[i];
+        }
+    }
+
     public static void main(String[] args) {
         ReverseString reverseString = new ReverseString();
-        System.out.println(reverseString.reverseStringBruteForce(new char[]{'h', 'e', 'l', 'l', 'o'}));
+        System.out.println(reverseString.reverseStringBetter(new char[]{'h', 'e', 'l', 'l', 'o'}));
     }
 }
