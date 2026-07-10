c1f1eda435dc48297326c8e9ceac2dae82dc1d68
Mohd Sameer <mohdsameer68257@gmail.com>
2026-07-10 20:13:20 +0530

    chore: learn the interor remove methods

--- 03_advJava/Iterator/Iterable.java
+++ 03_advJava/Iterator/Iterable.java
@@ -1,15 +1,23 @@
 package Iterator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Iterable {
     public static void main(String[] args) {
-        List<String> names = List.of("Sam", "John", "Alex");
+        List<String> names = new ArrayList<>(List.of("Sam", "John", "Alex"));
         Iterator<String> iterator = names.iterator();
 
+        // while (iterator.hasNext()) {
+        // System.out.println(iterator.next());
+        // }
+        // for (String name : names) {
+        // names.remove(name);
+        // }
         while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+            iterator.next();
+            iterator.remove();
         }
     }
 }
