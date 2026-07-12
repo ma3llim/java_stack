28dcd22cf932e7c0efa5ba155e5be49915ef083a
Mohd Sameer <mohdsameer68257@gmail.com>
2026-07-12 11:33:50 +0530

    chore: revision of the bean

---
+++ 10_spring_boot_web/project_1/app/src/main/java/org/example/service/UserService.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+import org.example.Bean.UserConfig;
+
+public class UserService {
+    UserConfig userConfig;
+
+    public UserService(UserConfig userConfig) {
+        this.userConfig = userConfig;
+    }
+}
