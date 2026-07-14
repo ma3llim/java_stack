0005da9a11c29695dc70c1bef70382f26031adbc
Mohd Sameer <mohdsameer68257@gmail.com>
2026-07-14 22:40:34 +0530

    feat: define the entities for users

---
+++ 11_projects/7_expense_tracker/userService/app/src/main/java/org/example/entities/UserInfo.java
@@ -0,0 +1,41 @@
+package org.example.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@Table(name = "users")
+public class UserInfo {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Id
+    @JsonProperty("user_id")
+    @NonNull
+    private String userId;
+
+    @JsonProperty("first_name")
+    @NonNull
+    private String firstName;
+
+    @JsonProperty("last_name")
+    @NonNull
+    private String lastName;
+
+    @JsonProperty("phone_number")
+    @NonNull
+    private Long phoneNumber;
+
+    @JsonProperty("email")
+    @NonNull
+    private String email;
+
+    @JsonProperty("profile_pic")
+    private String profilePic;
+}
