52121e55ca414283430ce2c4887f53a935322df5
Mohd Sameer <mohdsameer68257@gmail.com>
2026-07-17 12:33:30 +0530

    chore: logger in dsService

--- 11_projects/7_expense_tracker/dsService/src/app.py
+++ 11_projects/7_expense_tracker/dsService/src/app.py
@@ -1,11 +1,22 @@
+import json
+import os
 from flask import Flask
 from flask import request
 from flask import jsonify
 from service.MessageService import MessageService
+from kafka import KafkaProducer
+import logging
 
 app = Flask(__name__)
-
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 message_service = MessageService()
+
+producer = KafkaProducer(
+    bootstrap_servers=os.getenv("BOOTSTRAP_SERVERS"),
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
 
 @app.route("/v1/ds/message", methods=["POST"])
 def handle_message():
@@ -13,11 +24,19 @@ def handle_message():
     message = data.get("message")
 
     if not message:
-        return jsonify(
-            {"error": "Message Is Required"}
-        ), 400
+        return jsonify({"error": "Message Is Required"}), 400
     
     result = message_service.process_message(message)
+    try:
+        response = producer.send("expense-events",value=result)
+        metadata = response.get(timeout=10)
+
+        logger.info("Kafka message sent successfully | topic=%s partition=%s offset=%s",metadata.topic,metadata.partition,metadata.offset)
+        producer.flush()
+
+    except Exception as e:
+        logger.exception("Failed to publish message to Kafka topic expense-events")
+        return jsonify({"error": str(e)}), 500
 
     return jsonify(result)
 
