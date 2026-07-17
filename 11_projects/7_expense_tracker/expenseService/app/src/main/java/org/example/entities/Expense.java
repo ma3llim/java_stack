a02b5763cec1908e0b4d77edf5a2d576226333c4
Mohd Sameer <mohdsameer68257@gmail.com>
2026-07-17 13:13:23 +0530

    fix: created_at null value issue in DB

--- 11_projects/7_expense_tracker/expenseService/app/src/main/java/org/example/entities/Expense.java
+++ 11_projects/7_expense_tracker/expenseService/app/src/main/java/org/example/entities/Expense.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -32,6 +33,7 @@ public class Expense {
     private String merchant;
     @Column(name = "currency")
     private String currency;
+    @CreationTimestamp
     @Column(name = "created_at")
     private Timestamp createdAt;
 
