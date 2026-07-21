f0537ea6534181a6edccc7f54ba927fd10ba877f
Mohd Sameer <mohdsameer68257@gmail.com>
2026-07-21 14:27:07 +0530

    refactor: set-up lombok autowired instead of normal

--- 10_spring_boot_web/expection_handling/app/src/main/java/org/example/service/LoggerEmployeeService.java
+++ 10_spring_boot_web/expection_handling/app/src/main/java/org/example/service/LoggerEmployeeService.java
@@ -1,13 +1,11 @@
 package org.example.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class LoggerEmployeeService {
-    private static final Logger log = LoggerFactory.getLogger(LoggerEmployeeService.class);
-
     public void addEmployeeLogger() {
         log.info("Added New Employee");
     }
