17070a0293599dd8aa6cb224052b02dba1d07897
Mohd Sameer <mohdsameer68257@gmail.com>
2026-07-26 00:32:43 +0530

    docs: configure OpenAPI groups and JWT security documentation

--- 11_projects/9_authentication/app/src/main/java/org/example/config/swagger/APIDocConfig.java
+++ 11_projects/9_authentication/app/src/main/java/org/example/config/swagger/APIDocConfig.java
@@ -6,13 +6,15 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
                 title = "Auth Application",
-                description = "Generic auth app that can be use with any application",
+                description = "Generic auth application that can be used with any application",
                 contact = @Contact(
                         name = "Sameer",
                         email = "mohdsameer68257@gmail.com"
@@ -30,7 +32,17 @@ import org.springframework.context.annotation.Configuration;
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
-        bearerFormat = "JWT"
+        bearerFormat = "JWT",
+        description = "JWT authentication"
 )
 public class APIDocConfig {
+    @Bean
+    public GroupedOpenApi authApi() {
+        return GroupedOpenApi.builder().group("Authentication").pathsToMatch("/api/v1/auth/**").build();
+    }
+
+    @Bean
+    public GroupedOpenApi usersApi() {
+        return GroupedOpenApi.builder().group("Users").pathsToMatch("/api/v1/users/**").build();
+    }
 }
