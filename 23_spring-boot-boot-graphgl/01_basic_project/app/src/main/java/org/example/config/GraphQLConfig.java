744670ebf3588946028fc18486a7823ae73ab180
Mohd Sameer <mohdsameer68257@gmail.com>
2026-08-04 17:29:16 +0530

    feat: added query complexing limiting

--- 23_spring-boot-boot-graphgl/01_basic_project/app/src/main/java/org/example/config/GraphQLConfig.java
+++ 23_spring-boot-boot-graphgl/01_basic_project/app/src/main/java/org/example/config/GraphQLConfig.java
@@ -1,15 +1,25 @@
 package org.example.config;
 
+import graphql.analysis.MaxQueryComplexityInstrumentation;
 import graphql.analysis.MaxQueryDepthInstrumentation;
+import graphql.execution.instrumentation.ChainedInstrumentation;
 import graphql.execution.instrumentation.Instrumentation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 
 @Configuration
 public class GraphQLConfig {
     @Bean
-    public Instrumentation instrumentation() {
-        return new MaxQueryDepthInstrumentation(3);
+    public Instrumentation graphQLInstrumentation() {
+
+        return new ChainedInstrumentation(
+                List.of(
+                        new MaxQueryDepthInstrumentation(4),
+                        new MaxQueryComplexityInstrumentation(200)
+                )
+        );
     }
 }
